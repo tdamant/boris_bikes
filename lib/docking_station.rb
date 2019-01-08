9e1bf9dc9a557c998c958e68e7780fa6a993a2b6
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-08 17:38:48 +0000

    Add attribute reader to see docked bikes.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 class DockingStation
+  attr_reader :docked_bikes
+
+  def initialize
+    @docked_bikes = []
+  end
 
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @docked_bikes << bike
   end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -6,4 +6,9 @@ describe DockingStation do
     expect(subject.release_bike).to be_instance_of(Bike)
     expect(subject.release_bike.working?).to eq true
   end
+  it {is_expected.to respond_to :dock}
+  #when we call dock on a station, check the docked_bikes array is not empty
+  it "should not return an empty array once .dock has been called" do
+    expect(subject.dock(Bike.new)).to #not be empty!
+  end
 end
