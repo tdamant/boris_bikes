8340e06ed365791ce2c6aa7ba0287461009a6423
Szilvi Horvath <szil.horvath@gmail.com>
2019-01-08 16:40:30 +0000

    Test for docking stations releasing bikes

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it "should return an instance of the Bike class when .release_bike method is called" do
+    expect(subject.release_bike).to be_instance_of(Bike)
+    expect(subject.release_bike.working?).to eq true
+  end
 end
