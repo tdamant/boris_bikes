30e07904a9994e8a64a5ff987dff83ed49a7e186
Tom Damant <tom@gluon.local>
2019-01-10 16:56:05 +0000

    made methods to allow van to drop off bikes

--- lib/van.rb
+++ lib/van.rb
@@ -2,8 +2,20 @@ require_relative 'docking_station'
 
 class Van
 
+  attr_reader :bikes_in_van
+
+  def initialize
+    @bikes_in_van = []
+  end
+
   def pick_up_bikes(station)
-    station.docked_bikes.select { |bike| !bike.condition }
+    station.docked_bikes.each { |bike| @bikes_in_van << bike if !bike.condition }
+    self
+  end
+
+  def drop_off_bikes(garage)
+    @bikes_in_van = []
+    self
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,13 +4,20 @@ describe Van do
   let(:working_bike) { double(:condition => true) }
   let(:broken_bike) { double(:condition => false) }
   let(:station) { double(:docked_bikes => [working_bike, broken_bike])}
+  let(:garage) { double :garage }
 
   it "can load broken bikes from docking stations" do
     expect(subject).to respond_to :pick_up_bikes
   end
 
   it "calling the pick_up_bikes method will load the van with broken bikes" do
-    expect(subject.pick_up_bikes(station)).to eq([broken_bike])
+    van = subject.pick_up_bikes(station)
+    expect(van.bikes_in_van).to eq([broken_bike])
   end
-  
+
+  it "can drop off broken bikes at a garage" do
+    van = subject.pick_up_bikes(station)
+    expect(van.drop_off_bikes(garage).bikes_in_van ).to eq([])
+  end
+
 end
