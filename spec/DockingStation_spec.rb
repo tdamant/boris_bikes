efb2cba3c5dbeee66669b7f2e46b3fb99e0b56e1
Tom Damant <tom@gluon.local>
2019-01-10 15:32:24 +0000

    refactored tests to use doubles

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,30 +2,32 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let(:working_bike) { double(:condition => true) }
+  let(:broken_bike) { double(:condition => false) }
+
   it "Passing an interger to DockingStation.new should change the capacity of the docking station" do
-    station = DockingStation.new(12)
-    expect(station.capacity).to eq 12
+    expect(DockingStation.new(12).capacity).to eq 12
   end
 
   it "If we don't pass an argument to DockingStation.new the capacity should = 20" do
-    station = DockingStation.new
-    expect(station.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 
   it "should return an instance of the Bike class when .release_bike method is called" do
-    station = subject.dock(Bike.new)
-    expect(station.release_bike).to be_instance_of(Bike)
+    #allow(bike).to receive(:condition).and_return(true)
+    station = subject.dock(working_bike)
+    expect(station.release_bike).to be_instance_of(working_bike.class)
   end
 
   it "bikes should be working when released" do
-    station = subject.dock(Bike.new)
+    #allow(bike).to receive(:condition).and_return(true)
+    station = subject.dock(working_bike)
     expect(station.release_bike.condition).to eq true
   end
 
   it "docked_bikes should return a bike, if there is one" do
-    bike = Bike.new
-    station = subject.dock(bike)
-    expect(station.docked_bikes.include?(bike)).to eq true
+    station = subject.dock(working_bike)
+    expect(station.docked_bikes.include?(working_bike)).to eq true
   end
 
   it 'throws an error when trying to release bike and no bikes in station' do
@@ -33,14 +35,13 @@ describe DockingStation do
   end
 
   it "the program won't allow us to add more bikes than the station has capacity for" do
-    station = subject
-    expect{ (station.capacity).times { station.dock(Bike.new) } }.to_not raise_error
-    expect{ station.dock(Bike.new) }.to raise_error "dock is full"
+    expect{ (subject.capacity).times { subject.dock(working_bike) } }.to_not raise_error
+    expect{ subject.dock(working_bike) }.to raise_error "dock is full"
   end
 
   it "the dock should not release a bike if it's broken" do
-    bike = Bike.new
-    station = subject.dock(bike.is_broken)
+    #allow(bike).to receive(:condition).and_return(false)
+    station = subject.dock(broken_bike)
     expect{ station.release_bike }.to raise_error "bike is broken"
   end
 
